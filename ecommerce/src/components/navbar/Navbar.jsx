b9ded2fb71e47b4ca4edff85d62feac4ec293c8c
patitapaban <patitapabanpanda046@gmail.com>
2024-05-25 12:39:01 +0530

    create searcbar

--- ecommerce/src/components/navbar/Navbar.jsx
+++ ecommerce/src/components/navbar/Navbar.jsx
@@ -49,7 +49,7 @@ const Navbar = () => {
         <div className="left py-3 lg:py-0">
           <Link to={"/"}>
             <h2 className=" font-bold text-white text-2xl text-center">
-              E-Bharat
+              E-Shop
             </h2>
           </Link>
         </div>
