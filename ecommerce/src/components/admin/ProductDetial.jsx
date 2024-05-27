c3f81078e8df7b642aafdd109e9a1d2d6eed01c4
patitapaban <patitapabanpanda046@gmail.com>
2024-05-27 15:49:27 +0530

    add product for admin

--- ecommerce/src/components/admin/ProductDetial.jsx
+++ ecommerce/src/components/admin/ProductDetial.jsx
@@ -1,6 +1,12 @@
+import { useContext } from "react";
 import { Link } from "react-router-dom";
+import myContext from "../../context/myContext";
+import Loader from "../loader/Loader";
 
 const ProductDetail = () => {
+  const context = useContext(myContext);
+  const { loading, getAllProduct } = context;
+
   return (
     <div>
       <div className="py-5 flex justify-between items-center">
@@ -14,8 +20,12 @@ const ProductDetail = () => {
         </Link>
       </div>
 
+      <div className="flex justify-center relative top-20">
+        {loading && <Loader />}
+      </div>
+
       {/* table  */}
-      <div className="w-full overflow-x-auto">
+      <div className="w-full overflow-x-auto mb-5">
         <table className="w-full text-left border border-collapse sm:border-separate border-pink-100 text-pink-400">
           <tbody>
             <tr>
@@ -27,9 +37,33 @@ const ProductDetail = () => {
               </th>
               <th
                 scope="col"
+                className="h-12 px-6 text-md border-l first:border-l-0 border-pink-100 text-slate-700 bg-slate-100 font-bold fontPara"
+              >
+                Image
+              </th>
+              <th
+                scope="col"
                 className="h-12 px-6 text-md font-bold fontPara border-l first:border-l-0 border-pink-100 text-slate-700 bg-slate-100"
               >
-                Location Name
+                Title
+              </th>
+              <th
+                scope="col"
+                className="h-12 px-6 text-md font-bold fontPara border-l first:border-l-0 border-pink-100 text-slate-700 bg-slate-100"
+              >
+                Price
+              </th>
+              <th
+                scope="col"
+                className="h-12 px-6 text-md font-bold fontPara border-l first:border-l-0 border-pink-100 text-slate-700 bg-slate-100"
+              >
+                Category
+              </th>
+              <th
+                scope="col"
+                className="h-12 px-6 text-md font-bold fontPara border-l first:border-l-0 border-pink-100 text-slate-700 bg-slate-100"
+              >
+                Date
               </th>
               <th
                 scope="col"
@@ -44,20 +78,41 @@ const ProductDetail = () => {
                 Action
               </th>
             </tr>
-            <tr className="text-pink-300">
-              <td className="h-12 px-6 text-md transition duration-300 border-t border-l first:border-l-0 border-pink-100 stroke-slate-500 text-slate-500 ">
-                1.
-              </td>
-              <td className="h-12 px-6 text-md transition duration-300 border-t border-l first:border-l-0 border-pink-100 stroke-slate-500 text-slate-500 first-letter:uppercase ">
-                {"name"}
-              </td>
-              <td className="h-12 px-6 text-md transition duration-300 border-t border-l first:border-l-0 border-pink-100 stroke-slate-500 text-slate-500 text-green-500 cursor-pointer ">
-                Edit
-              </td>
-              <td className="h-12 px-6 text-md transition duration-300 border-t border-l first:border-l-0 border-pink-100 stroke-slate-500 text-slate-500 text-red-500 cursor-pointer ">
-                Delete
-              </td>
-            </tr>
+            {getAllProduct.map((item, index) => {
+              const { id, title, price, category, date, productImageUrl } =
+                item;
+
+              return (
+                <tr key={index} className="text-pink-300">
+                  <td className="h-12 px-6 text-md transition duration-300 border-t border-l first:border-l-0 border-pink-100 stroke-slate-500 text-slate-500 ">
+                    {index + 1}.
+                  </td>
+                  <td className="h-12 px-6 text-md transition duration-300 border-t border-l first:border-l-0 border-pink-100 stroke-slate-500 text-slate-500 first-letter:uppercase ">
+                    <div className="flex justify-center">
+                      <img className="w-20" src={productImageUrl} alt="" />
+                    </div>
+                  </td>
+                  <td className="h-12 px-6 text-md transition duration-300 border-t border-l first:border-l-0 border-pink-100 stroke-slate-500 text-slate-500 first-letter:uppercase ">
+                    {title}
+                  </td>
+                  <td className="h-12 px-6 text-md transition duration-300 border-t border-l first:border-l-0 border-pink-100 stroke-slate-500 text-slate-500 first-letter:uppercase ">
+                    ₹{price}
+                  </td>
+                  <td className="h-12 px-6 text-md transition duration-300 border-t border-l first:border-l-0 border-pink-100 stroke-slate-500 text-slate-500 first-letter:uppercase ">
+                    {category}
+                  </td>
+                  <td className="h-12 px-6 text-md transition duration-300 border-t border-l first:border-l-0 border-pink-100 stroke-slate-500 text-slate-500 first-letter:uppercase ">
+                    {date}
+                  </td>
+                  <td className="h-12 px-6 text-md transition duration-300 border-t border-l first:border-l-0 border-pink-100 stroke-slate-500 text-slate-500 text-green-500 cursor-pointer ">
+                    Edit
+                  </td>
+                  <td className="h-12 px-6 text-md transition duration-300 border-t border-l first:border-l-0 border-pink-100 stroke-slate-500 text-slate-500 text-red-500 cursor-pointer ">
+                    Delete
+                  </td>
+                </tr>
+              );
+            })}
           </tbody>
         </table>
       </div>
--- ecommerce/src/context/myState.jsx
+++ ecommerce/src/context/myState.jsx
@@ -1,14 +1,48 @@
-import { useState } from "react";
+/* eslint-disable react/prop-types */
+import { useEffect, useState } from "react";
 import MyContext from "./myContext";
+import { collection, onSnapshot, orderBy, query } from "firebase/firestore";
+import { fireDB } from "../firebase/FirebaseConfig";
 
 function MyState({ children }) {
+  // Loading State
   const [loading, setLoading] = useState(false);
-  const name = "Patitapaban Panda";
+
+  // User State
+  const [getAllProduct, setGetAllProduct] = useState([]);
+
+  /**========================================================================
+   *                          GET All Product Function
+   *========================================================================**/
+
+  const getAllProductFunction = async () => {
+    setLoading(true);
+    try {
+      const q = query(collection(fireDB, "product"), orderBy("time"));
+      const data = onSnapshot(q, (QuerySnapshot) => {
+        let productArray = [];
+        QuerySnapshot.forEach((doc) => {
+          productArray.push({ ...doc.data(), id: doc.id });
+        });
+        setGetAllProduct(productArray);
+        setLoading(false);
+      });
+      return () => data;
+    } catch (error) {
+      console.log(error);
+      setLoading(false);
+    }
+  };
+
+  useEffect(() => {
+    getAllProductFunction();
+  }, []);
   return (
     <MyContext.Provider
       value={{
         loading,
         setLoading,
+        getAllProduct,
       }}
     >
       {children}
