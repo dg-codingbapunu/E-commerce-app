ada1c26ff4e90e85fc0b1ae281028f56dfbb807f
patitapaban <patitapabanpanda046@gmail.com>
2024-05-28 09:50:01 +0530

    get product on home page

--- ecommerce/src/components/homePageProductCard/HomePageProductCard.jsx
+++ ecommerce/src/components/homePageProductCard/HomePageProductCard.jsx
@@ -1,91 +1,12 @@
+import { useContext } from "react";
 import { useNavigate } from "react-router-dom";
-
-// productData
-const productData = [
-  {
-    id: 1,
-    image:
-      "https://i.pinimg.com/564x/3e/05/ce/3e05cefbc7eec79ac175ea8490a67939.jpg",
-    title: "Hand Painted Blue Kaushalam Tea Pot in Aluminium",
-    desc: "Shop Hand Painted Blue Kaushalam Tea Pot in Aluminium, handmade by Mrinalika Jain. Fair pricing. Ethically made. Positive impact.",
-    price: 350,
-    trendingProductName: "Featured",
-    quantity: 1,
-  },
-  {
-    id: 2,
-    image:
-      "https://i.pinimg.com/736x/e4/61/f2/e461f2246b6ad93e2099d98780626396.jpg",
-    title: "Kaushalam kalash Copper Pot",
-    desc: "Shop Hand Painted Blue Kaushalam Tea Pot in Aluminium, handmade by Mrinalika Jain. Fair pricing. Ethically made. Positive impact.",
-    price: 290,
-    trendingProductName: "Featured",
-    quantity: 1,
-  },
-  {
-    id: 3,
-    image:
-      "https://i.pinimg.com/564x/fd/50/68/fd50688767adb47aba7204f034554cbd.jpg",
-    title: "Hand Painted Blue Kaushalam Tea Pot in Aluminium",
-    desc: "Shop Hand Painted Blue Kaushalam Tea Pot in Aluminium, handmade by Mrinalika Jain. Fair pricing. Ethically made. Positive impact.",
-    price: 100,
-    trendingProductName: "Featured",
-    quantity: 1,
-  },
-  {
-    id: 4,
-    image:
-      "https://i.pinimg.com/564x/22/80/8d/22808d88ada424962f2e064f3075b2d1.jpg",
-    title: "Hand Painted Blue Kaushalam Tea Pot in Aluminium",
-    desc: "Shop Hand Painted Blue Kaushalam Tea Pot in Aluminium, handmade by Mrinalika Jain. Fair pricing. Ethically made. Positive impact.",
-    price: 99,
-    trendingProductName: "Featured",
-    quantity: 1,
-  },
-  {
-    id: 1,
-    image:
-      "https://i.pinimg.com/564x/3e/05/ce/3e05cefbc7eec79ac175ea8490a67939.jpg",
-    title: "Hand Painted Blue Kaushalam Tea Pot in Aluminium",
-    desc: "Shop Hand Painted Blue Kaushalam Tea Pot in Aluminium, handmade by Mrinalika Jain. Fair pricing. Ethically made. Positive impact.",
-    price: 150,
-    trendingProductName: "Featured",
-    quantity: 1,
-  },
-  {
-    id: 2,
-    image:
-      "https://i.pinimg.com/736x/e4/61/f2/e461f2246b6ad93e2099d98780626396.jpg",
-    title: "Kaushalam kalash Copper Pot",
-    desc: "Shop Hand Painted Blue Kaushalam Tea Pot in Aluminium, handmade by Mrinalika Jain. Fair pricing. Ethically made. Positive impact.",
-    price: 120,
-    trendingProductName: "Featured",
-    quantity: 1,
-  },
-  {
-    id: 3,
-    image:
-      "https://i.pinimg.com/564x/fd/50/68/fd50688767adb47aba7204f034554cbd.jpg",
-    title: "Hand Painted Blue Kaushalam Tea Pot in Aluminium",
-    desc: "Shop Hand Painted Blue Kaushalam Tea Pot in Aluminium, handmade by Mrinalika Jain. Fair pricing. Ethically made. Positive impact.",
-    price: 130,
-    trendingProductName: "Featured",
-    quantity: 1,
-  },
-  {
-    id: 4,
-    image:
-      "https://i.pinimg.com/564x/22/80/8d/22808d88ada424962f2e064f3075b2d1.jpg",
-    title: "Hand Painted Blue Kaushalam Tea Pot in Aluminium",
-    desc: "Shop Hand Painted Blue Kaushalam Tea Pot in Aluminium, handmade by Mrinalika Jain. Fair pricing. Ethically made. Positive impact.",
-    price: 120,
-    trendingProductName: "Featured",
-    quantity: 1,
-  },
-];
+import myContext from "../../context/myContext";
+import Loader from "../loader/Loader";
 
 const HomePageProductCard = () => {
   const navigate = useNavigate();
+  const context = useContext(myContext);
+  const { getAllProduct, loading } = context;
   return (
     <div className="mt-10">
       {/* Heading  */}
@@ -99,17 +20,18 @@ const HomePageProductCard = () => {
       <section className="text-gray-600 body-font">
         {/* main 2 */}
         <div className="container px-5 py-5 mx-auto">
+          <div className="flex justify-center">{loading && <Loader />}</div>
           {/* main 3 */}
           <div className="flex flex-wrap -m-4">
-            {productData.map((item, index) => {
-              const { image, title, price } = item;
+            {getAllProduct.slice(0, 8).map((item, index) => {
+              const { id, title, price, productImageUrl } = item;
               return (
                 <div key={index} className="p-4 w-full md:w-1/4">
                   <div className="h-full border border-gray-300 rounded-xl overflow-hidden shadow-md cursor-pointer">
                     <img
                       onClick={() => navigate("/productinfo")}
                       className="lg:h-80  h-96 w-full"
-                      src={image}
+                      src={productImageUrl}
                       alt="blog"
                     />
                     <div className="p-6">
